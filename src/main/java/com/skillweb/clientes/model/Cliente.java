abc817749969ba51ba0539b2be1c1408b4fefcde
alexander134 <alexanderdiaz_134@hotmail.com>
2022-12-20 18:23:13 -0300

    cambion de nombre columna

--- src/main/java/com/skillweb/clientes/model/Cliente.java
+++ src/main/java/com/skillweb/clientes/model/Cliente.java
@@ -6,15 +6,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+//import org.springframework.data.annotation.PersistenceCreator;
+
+//import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="cliente")
 public class Cliente {
-
+	/*
+	@PersistenceCreator
+	@JsonCreator
+	public Cliente(Long identificador, String nombre, String apellido, String telefono) {
+		super();
+		this.identificador = identificador;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+	}*/
+	
+	public Cliente(){}
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long identificador;
